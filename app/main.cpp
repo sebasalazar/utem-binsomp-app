fe8ceec819aa9baaaf1291c6c897d86449757daf
Sebastián Salazar <sebasalazar@gmail.com>
2021-07-03 10:51:06 -0400

    Generación del archivo principal

---
+++ app/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+/**
+ * 
+ * @param argc Cantidad de argumentos que reciben por la línea de comandos.
+ * @param argv Matriz asociativa con los argumentos.
+ * @return Estado de terminación de la aplicación (0 es éxito), 
+ * cualquier otro valor es asumido como error de ejecución por el sistema operativo.
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
